484a3395c16704e4f54f12a42581f12873a4be13
Dann Wee <dannweezj@gmail.com>
2025-05-14 21:02:08 +0800

    fix: comment

--- packages/dynamic-bonding-curve/src/services/pool.ts
+++ packages/dynamic-bonding-curve/src/services/pool.ts
@@ -367,7 +367,7 @@ export class PoolService extends DynamicBondingCurveProgram {
                     NATIVE_MINT.toBase58()
                 )
             ) {
-                const unwrapIx = unwrapSOLInstruction(poolCreator)
+                const unwrapIx = unwrapSOLInstruction(poolCreator, poolCreator)
                 unwrapIx && postInstructions.push(unwrapIx)
             }
 
@@ -463,7 +463,7 @@ export class PoolService extends DynamicBondingCurveProgram {
                 NATIVE_MINT.toBase58()
             )
         ) {
-            const unwrapIx = unwrapSOLInstruction(owner)
+            const unwrapIx = unwrapSOLInstruction(owner, owner)
 
             unwrapIx && postInstructions.push(unwrapIx)
         }
