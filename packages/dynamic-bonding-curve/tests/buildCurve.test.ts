47a06aa85d1e0a67238f024915661720e2c3ab5f
Dann Wee <dannweezj@gmail.com>
2025-05-22 14:21:25 +0800

    fix: build curve with two segments function

--- packages/dynamic-bonding-curve/tests/buildCurve.test.ts
+++ packages/dynamic-bonding-curve/tests/buildCurve.test.ts
@@ -345,16 +345,14 @@ describe('buildCurve tests', () => {
     test('build curve with two segments', () => {
         console.log('\n testing build curve with two segments...')
 
-        const curveGraphParams = {
-            ...baseParams,
+        const config = buildCurveWithTwoSegments({
             totalTokenSupply: 1000000000,
-            initialMarketCap: 20,
-            migrationMarketCap: 320,
+            initialMarketCap: 200000,
+            migrationMarketCap: 1000000,
             percentageSupplyOnMigration: 20,
-            tokenBaseDecimal: TokenDecimal.SIX,
-            tokenQuoteDecimal: TokenDecimal.NINE,
-            leftover: 350000000,
             migrationOption: MigrationOption.MET_DAMM_V2,
+            tokenBaseDecimal: TokenDecimal.NINE,
+            tokenQuoteDecimal: TokenDecimal.NINE,
             lockedVestingParam: {
                 totalLockedVestingAmount: 0,
                 amountPerVestingPeriod: 0,
@@ -362,15 +360,39 @@ describe('buildCurve tests', () => {
                 totalVestingDuration: 0,
                 cliffDurationFromMigrationTime: 0,
             },
-        }
-
-        const config = buildCurveWithTwoSegments(curveGraphParams)
+            feeSchedulerParam: {
+                startingFeeBps: 5000,
+                endingFeeBps: 100,
+                numberOfPeriod: 120,
+                totalDuration: 120,
+                feeSchedulerMode: FeeSchedulerMode.Exponential,
+            },
+            dynamicFeeEnabled: true,
+            activationType: ActivationType.Slot,
+            collectFeeMode: CollectFeeMode.OnlyQuote,
+            migrationFeeOption: MigrationFeeOption.FixedBps100,
+            tokenType: TokenType.SPL,
+            partnerLpPercentage: 0,
+            creatorLpPercentage: 0,
+            partnerLockedLpPercentage: 100,
+            creatorLockedLpPercentage: 0,
+            creatorTradingFeePercentage: 0,
+            leftover: 350000000,
+        })
 
         console.log(
             'migrationQuoteThreshold: %d',
             config.migrationQuoteThreshold
                 .div(new BN(10 ** TokenDecimal.NINE))
                 .toString()
+        )
+        console.log(
+            'baseFeeParams',
+            convertBNToDecimal(config.poolFees.baseFee)
+        )
+        console.log(
+            'lockedVestingParams',
+            convertBNToDecimal(config.lockedVesting)
         )
         console.log('sqrtStartPrice', convertBNToDecimal(config.sqrtStartPrice))
         console.log('curve', convertBNToDecimal(config.curve))
